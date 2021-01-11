4267b4747b1ea2002c563d785738ca8d98f8be43
Bruna-Pianco <bruna.magalhaes02@hotmail.com>
2021-01-11 11:12:01 -0300

    commit inicial

---
+++ Arquivos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace divisao
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            {
+                Console.WriteLine("Informe 2 números para realizar a divisão, será realizada do primeiro número pelo segundo número!:\n");
+                Console.WriteLine("Informe o primeiro número para realizar a divisão:");
+                x = double.Parse(Console.ReadLine());
+                Console.WriteLine("Informe o número divisor:");
+                y = double.Parse(Console.ReadLine());
+
+                if (y == 0)
+                {
+                    Console.WriteLine("Divisão impossivel, escolha outro número divisor!");
+                }
+
+                else
+                {
+                    Console.WriteLine("O Resultado da divisão é " + x / y);
+                }
+                Console.ReadKey();
+
+
+
+            }
+        }
+
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,8 @@
+##### Meu primeiro código.
+
+###### É um código simples feito com o :heart:
+
+###### Resolvendo contas de divisão.
+
+- ###### Desenvolvido em C# no Visual Studio 2019.
+
